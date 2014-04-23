03ce04f9dae8a0bf37ce17de8decf9688a382cf1
casa <casa@sweetohm.net>
2014-04-24 01:22:48 +0200

    Refactoring

--- src/github.com/c4s4/gontinuum/gontinuum.go
+++ src/github.com/c4s4/gontinuum/gontinuum.go
@@ -33,52 +33,27 @@ import (
 	"time"
 )
 
+///////////////////////////////////////////////////////////////////////////////
+//                              CONFIG STUFF                                 //
+///////////////////////////////////////////////////////////////////////////////
+
+type EmailConfig struct {
+	SmtpHost  string "smtp_host"
+	Recipient string
+	Sender    string
+	Success   bool
+}
+
+type ModuleConfig struct {
+	Name    string
+	Url     string
+	Command string
+}
+
 type Config struct {
 	Directory string
-	Email     struct {
-		SmtpHost  string "smtp_host"
-		Recipient string
-		Sender    string
-		Success   bool
-	}
-	Modules []struct {
-		Name    string
-		Url     string
-		Command string
-	}
-}
-
-type Build struct {
-	Module  string
-	Success bool
-	Output  string
-}
-
-func (build Build) String() string {
-	if build.Success {
-		return fmt.Sprintf("%s: OK", build.Module)
-	} else {
-		return fmt.Sprintf("%s: ERROR", build.Module)
-	}
-}
-
-type Builds []Build
-
-func (builds Builds) Success() bool {
-	for _, module := range builds {
-		if !module.Success {
-			return false
-		}
-	}
-	return true
-}
-
-func (builds Builds) String() string {
-	if builds.Success() {
-		return "SUCCESS"
-	} else {
-		return "FAILURE"
-	}
+	Email     EmailConfig
+	Modules   []ModuleConfig
 }
 
 func loadConfig(file string) Config {
@@ -100,14 +75,51 @@ func loadConfig(file string) Config {
 	return config
 }
 
-func buildModule(index int, config Config) Build {
-	fmt.Printf("Building '%s'... ", config.Modules[index].Name)
-	moduleDir := path.Join(config.Directory, config.Modules[index].Name)
+///////////////////////////////////////////////////////////////////////////////
+//                                BUILD STUFF                                //
+///////////////////////////////////////////////////////////////////////////////
+
+type Build struct {
+	Module  ModuleConfig
+	Success bool
+	Output  string
+}
+
+type Builds []Build
+
+func (build Build) String() string {
+	if build.Success {
+		return fmt.Sprintf("%s: OK", build.Module.Name)
+	} else {
+		return fmt.Sprintf("%s: ERROR", build.Module.Name)
+	}
+}
+
+func (builds Builds) Success() bool {
+	for _, build := range builds {
+		if !build.Success {
+			return false
+		}
+	}
+	return true
+}
+
+func (builds Builds) String() string {
+	if builds.Success() {
+		return "SUCCESS"
+	} else {
+		return "FAILURE"
+	}
+}
+
+func buildModule(module ModuleConfig, directory string) Build {
+	fmt.Printf("Building '%s'... ", module.Name)
+	moduleDir := path.Join(directory, module.Name)
 	// go in build directory
-	err := os.Chdir(config.Directory)
+	err := os.Chdir(directory)
 	if err != nil {
 		return Build{
-			Module:  config.Modules[index].Name,
+			Module:  module,
 			Success: false,
 			Output:  err.Error(),
 		}
@@ -117,32 +129,32 @@ func buildModule(index int, config Config) Build {
 		os.RemoveAll(moduleDir)
 	}
 	// git clone the module repository
-	cmd := exec.Command("git", "clone", config.Modules[index].Url)
+	cmd := exec.Command("git", "clone", module.Url)
 	output, err := cmd.CombinedOutput()
 	defer os.RemoveAll(moduleDir)
 	if err != nil {
 		fmt.Println("ERROR")
 		return Build{
-			Module:  config.Modules[index].Name,
+			Module:  module,
 			Success: false,
 			Output:  string(output),
 		}
 	} else {
 		os.Chdir(moduleDir)
 		// run the build command
-		cmd := exec.Command("bash", "-c", config.Modules[index].Command)
+		cmd := exec.Command("bash", "-c", module.Command)
 		output, err := cmd.CombinedOutput()
 		if err != nil {
 			fmt.Println("ERROR")
 			return Build{
-				Module:  config.Modules[index].Name,
+				Module:  module,
 				Success: false,
 				Output:  strings.TrimSpace(string(output)),
 			}
 		} else {
 			fmt.Println("OK")
 			return Build{
-				Module:  config.Modules[index].Name,
+				Module:  module,
 				Success: true,
 				Output:  string(output),
 			}
@@ -152,30 +164,30 @@ func buildModule(index int, config Config) Build {
 
 func buildModules(config Config) Builds {
 	builds := make(Builds, len(config.Modules))
-	for index, _ := range config.Modules {
-		builds[index] = buildModule(index, config)
+	for index, module := range config.Modules {
+		builds[index] = buildModule(module, config.Directory)
 	}
 	return builds
 }
 
 const timeFormat = "2006-01-02 15:04"
 
-func sendReport(builds Builds, start time.Time, duration time.Duration, config Config) {
+func sendEmail(builds Builds, start time.Time, duration time.Duration, config Config) {
 	if !builds.Success() || (builds.Success() && config.Email.Success) {
 		subject := fmt.Sprintf("Build on %s was a %s", start.Format(timeFormat), builds)
 		message := fmt.Sprintf("From: %s\n", config.Email.Sender)
 		message += fmt.Sprintf("To: %s\n", config.Email.Recipient)
 		message += fmt.Sprintf("Subject: %s\n\n", subject)
 		message += fmt.Sprintf("Build on %s:\n\n", start.Format(timeFormat))
-		for module := range builds {
-			message += fmt.Sprintf("  %s\n", builds[module].String())
+		for _, build := range builds {
+			message += fmt.Sprintf("  %s\n", build.String())
 		}
 		message += fmt.Sprintf("\nDone in %s\n", duration)
 		message += builds.String()
 		for _, build := range builds {
 			if !build.Success {
 				message += fmt.Sprintf("\n\n===================================\n")
-				message += fmt.Sprintf(build.Module)
+				message += fmt.Sprintf(build.Module.Name)
 				message += fmt.Sprintf("\n-----------------------------------\n")
 				message += fmt.Sprintf(build.Output)
 				message += fmt.Sprintf("\n-----------------------------------\n")
@@ -198,6 +210,6 @@ func main() {
 		duration := time.Since(start)
 		fmt.Println("Done in", duration)
 		fmt.Println(builds.String())
-		sendReport(builds, start, duration, config)
+		sendEmail(builds, start, duration, config)
 	}
 }
